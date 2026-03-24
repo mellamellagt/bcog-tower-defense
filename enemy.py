d98e46f3b00391edef4703c525a53904375e64bc
mellamellagt <kingdomofvortexia@gmail.com>
2026-03-24 10:26:25 -0500

    added basic program structure

---
+++ enemy.py
@@ -0,0 +1,5 @@
+import pygame as pg
+
+class Enemy(pg.sprite.Sprite):
+    def __init__(self):
+        super().__init__(self)
---
+++ main.py
@@ -0,0 +1,7 @@
+import pygame as pg
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
