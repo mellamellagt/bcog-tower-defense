91cd61137d2b8aef8972c721d9348fa28e7b8fa5
mellamellagt <kingdomofvortexia@gmail.com>
2026-03-31 09:33:17 -0500

    Enemy Logic

--- enemy.py
+++ enemy.py
@@ -1,5 +1,57 @@
 import pygame as pg
+from pygame.math import Vector2
+import math
 
 class Enemy(pg.sprite.Sprite):
-    def __init__(self):
-        super().__init__(self)
+    def __init__(self, waypoints, image):
+        # Sprite Init
+        pg.sprite.Sprite.__init__(self)
+        
+        # Regarding Waypoints
+        self.waypoints = waypoints
+        self.pos = Vector2(self.waypoints[0])
+        self.target_waypoint = 1
+
+        # Regarding Movement Speed
+        self.speed = 1
+        self.movement_remaining = 0
+
+        # Regarding Image
+        self.original_image = image
+        self.angle = 0
+        self.image = pg.transform.rotate(self.original_image, self.angle)
+        self.rect = self.image.get_rect()
+        self.rect.center = self.pos
+
+    def update(self):
+        self.move()
+        self.rotate()
+    
+    def move(self):
+        # Check if there is more 'track' to go
+        if self.target_waypoint < len(self.waypoints):
+            self.target = Vector2(self.waypoints[self.target_waypoint])
+            self.movement = self.target - self.pos
+        else:
+            self.kill()
+        # Perform 'overflow' movement
+        if self.movement_remaining != 0 and self.movement[0] >= self.movement_remaining[0] and self.movement[1] >= self.movement_remaining[1]:
+            self.pos += self.movement_remaining
+            self.movement_remaining = 0
+        # Perform Movement
+        dist = self.movement.length()
+        if dist >= self.speed: 
+            self.pos += self.movement.normalize() * self.speed
+        else:
+            if dist != 0: 
+                self.pos += self.movement.normalize()
+                self.movement_remaining = (self.movement.normalize() * self.speed) - self.movement.normalize()
+            self.target_waypoint += 1
+
+    def rotate(self):
+        # Calculate + Perform Rotation
+        dist = self.target - self.pos
+        self.angle = math.degrees(math.atan2(-dist[1], dist[0]))
+        self.image = pg.transform.rotate(self.original_image, self.angle)
+        self.rect = self.image.get_rect()
+        self.rect.center = self.pos
--- main.py
+++ main.py
@@ -1,7 +1,54 @@
 import pygame as pg
+from enemy import Enemy
+
+def run_game(screen_width, screen_height, FPS):
+    # Initialize Pygame
+    pg.init()
+
+    # Create Clock
+    clock = pg.time.Clock()
+
+    # Create Screen
+    screen = pg.display.set_mode((screen_width, screen_height))
+    pg.display.set_caption("BCOG 200 Tower Defense")
+
+    # TEMPORARY ENEMY TESTING
+    enemy_image = pg.image.load('assets\enemies\placeholder.png').convert_alpha()
+    waypoints = [
+        (100, 200),
+        (300, 100),
+        (500, 200),
+        (0, 0),
+        (600, 600)
+    ]
+    enemy_group = pg.sprite.Group()
+    enemy = Enemy(waypoints, enemy_image)
+    enemy_group.add(enemy)
+
+    run = True
+    while run: 
+        # Set FPS
+        clock.tick(FPS)
+
+        # Update Group(s)
+        enemy_group.update()
+
+        # Draw Stuff?
+        screen.fill("grey100")
+        enemy_group.draw(screen)
+        
+        # Event handler
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                run = False
+        
+        # Update Screen
+        pg.display.flip()
+    # Quit Pygame
+    pg.quit()
 
 def main():
-    pass
+    run_game(600, 600, 60)
 
 if __name__ == "__main__":
     main()
